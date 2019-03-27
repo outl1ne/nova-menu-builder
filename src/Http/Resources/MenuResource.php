c2a527cc6a58a1247b044768d0c248a89211675b
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-03-27 15:28:28 +0200

    Fix slug-locale composite unique key validation

--- src/Http/Resources/MenuResource.php
+++ src/Http/Resources/MenuResource.php
@@ -58,7 +58,7 @@ class MenuResource extends Resource
 
             Text::make(__('Slug'), 'slug')
                 ->sortable()
-                ->rules('required', 'max:255', 'unique:menus,slug,NULL,NULL,locale,' . $request->locale),
+                ->rules('required', 'max:255', 'unique:menus,slug,{{resourceId}},id,locale,' . $request->locale),
 
             Select::make(__('Locale'), 'locale')
                 ->options(MenuBuilder::getLocales())
