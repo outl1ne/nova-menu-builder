1cc7e43bf8e9ad4901c44792847217c406770c0f
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-04-16 10:57:06 +0300

    Improve nova_get_menus helper

--- src/helpers.php
+++ src/helpers.php
@@ -5,6 +5,30 @@ use OptimistDigital\MenuBuilder\Models\Menu;
 if (!function_exists('nova_get_menus')) {
     function nova_get_menus()
     {
-        return Menu::all()->load('rootMenuItems');
+        $formatMenuItem = function ($menuItem) use (&$formatMenuItem) {
+            return [
+                'id' => $menuItem->id,
+                'menuId' => $menuItem->menu_id,
+                'type' => $menuItem->class::getIdentifier(),
+                'value' => $menuItem->value,
+                'target' => $menuItem->target,
+                'parameters' => $menuItem->parameters,
+                'parentId' => $menuItem->parent_id,
+                'order' => $menuItem->order,
+                'enabled' => $menuItem->enabled,
+                'children' => empty($menuItem->children) ? [] : $menuItem->children->map($formatMenuItem),
+            ];
+        };
+
+        return Menu::all()
+            ->load('rootMenuItems')
+            ->map(function ($menu) use ($formatMenuItem) {
+                return [
+                    'id' => $menu->id,
+                    'name' => $menu->name,
+                    'slug' => $menu->slug,
+                    'menuItems' => collect($menu->rootMenuItems)->map($formatMenuItem),
+                ];
+            });
     }
 }
