60766011a148b8b5458a9a505f43221cfda26767
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-05-08 11:07:08 +0300

    Add nova_get_menu helper

--- src/Models/Menu.php
+++ src/Models/Menu.php
@@ -16,4 +16,32 @@ class Menu extends Model
             ->orderby('order')
             ->orderby('name');
     }
+
+    public function formatForAPI()
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'locale' => $this->locale,
+            'menuItems' => collect($this->rootMenuItems)->map(function ($item) {
+                return $this->formatMenuItem($item);
+            }),
+        ];
+    }
+
+    public function formatMenuItem($menuItem)
+    {
+        return [
+            'id' => $menuItem->id,
+            'type' => $menuItem->type,
+            'value' => $menuItem->customValue,
+            'target' => $menuItem->target,
+            'parameters' => $menuItem->parameters,
+            'enabled' => $menuItem->enabled,
+            'children' => empty($menuItem->children) ? [] : $menuItem->children->map(function ($item) {
+                return $this->formatMenuItem($item);
+            }),
+        ];
+    }
 }
--- src/helpers.php
+++ src/helpers.php
@@ -5,28 +5,22 @@ use OptimistDigital\MenuBuilder\Models\Menu;
 if (!function_exists('nova_get_menus')) {
     function nova_get_menus()
     {
-        $formatMenuItem = function ($menuItem) use (&$formatMenuItem) {
-            return [
-                'id' => $menuItem->id,
-                'type' => $menuItem->type,
-                'value' => $menuItem->customValue,
-                'target' => $menuItem->target,
-                'parameters' => $menuItem->parameters,
-                'enabled' => $menuItem->enabled,
-                'children' => empty($menuItem->children) ? [] : $menuItem->children->map($formatMenuItem),
-            ];
-        };
-
         return Menu::all()
             ->load('rootMenuItems')
-            ->map(function ($menu) use ($formatMenuItem) {
-                return [
-                    'id' => $menu->id,
-                    'name' => $menu->name,
-                    'slug' => $menu->slug,
-                    'locale' => $menu->locale,
-                    'menuItems' => collect($menu->rootMenuItems)->map($formatMenuItem),
-                ];
+            ->map(function ($menu) {
+                return $menu->formatForAPI();
             });
+    }
+}
+
+if (!function_exists('nova_get_menu')) {
+    function nova_get_menu($menuSlug)
+    {
+        if (empty($menuSlug)) return null;
+
+        $menu = Menu::whereSlug($menuSlug)->get()->first();
+        if (!isset($menu)) return null;
+
+        return $menu->formatForAPI();
     }
 }
