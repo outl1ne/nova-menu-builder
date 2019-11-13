c8a8bd80f3f04b4fed099692a403087af69c0e4a
Marttin Notta <marttin@optimistdigital.com>
2019-11-13 10:56:48 +0200

    Fix menu item storage keys

--- resources/js/components/BuilderResourceTool.vue
+++ resources/js/components/BuilderResourceTool.vue
@@ -110,18 +110,16 @@ export default {
     saveMenuLocalState() {
       if (!Array.isArray(this.menuItems) || this.menuItems.length === 0) return;
       const menuItemsState = this.menuItems.map(item => item && this.getCascadeState(item));
-      let menuStorage = this.getMenuLocalState();
-      if (!menuStorage) {
-        menuStorage = {};
-      }
-      menuStorage[this.resourceId] = menuItemsState;
+      const data = localStorage.getItem('menuManagerItemsState');
+      let menuStorage = data && JSON.parse(data) || {};
+      menuStorage[`resource-${this.resourceId}`] = menuItemsState;
       localStorage.setItem('menuManagerItemsState', JSON.stringify(menuStorage));
     },
 
     getMenuLocalState() {
       const menuStorage = JSON.parse(localStorage.getItem('menuManagerItemsState'));
-      if (!menuStorage || !menuStorage[this.resourceId]) return null;
-      return menuStorage[this.resourceId];
+      if (!menuStorage || !menuStorage[`resource-${this.resourceId}`]) return null;
+      return menuStorage[`resource-${this.resourceId}`];
     },
 
     getData() {
@@ -136,7 +134,8 @@ export default {
 
     setMenuItemProperties(menuItems, localItemsState = null) {
       return menuItems.map(item => {
-        const localItemState = Array.isArray(localItemsState) && localItemsState.find(localItem => +localItem.id === +item.id);
+        const localItemState = Array.isArray(localItemsState) ? localItemsState.find(localItem => +localItem.id === +item.id) : false;
+
         return {
           ...item,
           classProp: [localItemState && !localItemState.cascade ? 'hide-cascade' : ''],
