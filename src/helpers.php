2d07eb07ad2214c877947fa044ff11a3a7589fa0
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2020-05-25 15:35:11 +0300

    Fix nova_get_menu backwards compatibility

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,6 @@
 <?php
 
+use OptimistDigital\MenuBuilder\MenuBuilder;
 use OptimistDigital\MenuBuilder\Models\Menu;
 
 if (!function_exists('nova_get_menus')) {
@@ -14,9 +15,10 @@ if (!function_exists('nova_get_menus')) {
 }
 
 if (!function_exists('nova_get_menu')) {
-    function nova_get_menu($slug, $locale)
+    function nova_get_menu($slug, $locale = null)
     {
-        if (empty($slug) || empty($locale)) return null;
+        if (empty($slug)) return null;
+        if (empty($locale)) $locale = array_keys(MenuBuilder::getLocales())[0] ?? null;
         $menu = Menu::where('slug', $slug)->where('locale', $locale)->get()->first();
         return !empty($menu) ? $menu->formatForAPI() : null;
     }
