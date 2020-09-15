1e5ec87ac904aac8aeeae9f3437356c748e1d792
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2020-09-15 16:28:49 +0300

    MenuBuilderServiceProvider cleanup

--- src/MenuBuilderServiceProvider.php
+++ src/MenuBuilderServiceProvider.php
@@ -10,43 +10,34 @@ use OptimistDigital\NovaTranslationsLoader\NovaTranslationsLoader;
 
 class MenuBuilderServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
         // Load views
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'nova-menu');
+
+        // Load translations
+        NovaTranslationsLoader::loadTranslations(__DIR__ . '/../resources/lang', 'nova-menu-builder', true);
 
         // Load migrations
         if (config('nova-menu.auto_load_migrations', true)) {
             $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
         }
 
-        $this->app->booted(function () {
-            $this->routes();
-        });
-
         // Publish data
         $this->publishes([__DIR__ . '/../database/migrations' => database_path('migrations')], 'nova-menu-builder-migrations');
         $this->publishes([__DIR__ . '/../config' => config_path()], 'nova-menu-builder-config');
-
-        // Load translations
-        NovaTranslationsLoader::loadTranslations(__DIR__ . '/../resources/lang', 'nova-menu-builder', true);
 
         // Register resource
         Nova::resources([
             MenuBuilder::getMenuResource(),
         ]);
+
+        // Register routes
+        $this->app->booted(function () {
+            $this->routes();
+        });
     }
 
-    /**
-     * Register the tool's routes.
-     *
-     * @return void
-     */
     protected function routes()
     {
         if ($this->app->routesAreCached()) return;
@@ -55,26 +46,5 @@ class MenuBuilderServiceProvider extends ServiceProvider
             ->namespace('OptimistDigital\MenuBuilder\Http\Controllers')
             ->prefix('nova-vendor/nova-menu')
             ->group(__DIR__ . '/../routes/api.php');
-    }
-
-    /**
-     * Publish required migration
-     */
-    private function publishMigrations()
-    {
-    }
-
-    /**
-     * Publish sidebar menu item template
-     */
-    private function publishViews()
-    {
-    }
-
-    /**
-     * Publish config
-     */
-    private function publishConfig()
-    {
     }
 }
