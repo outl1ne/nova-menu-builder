fde9570f92a00a3331c9d566a4d963ebd1a3c232
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2020-09-17 14:28:54 +0300

    Remove unused code

--- src/Models/Menu.php
+++ src/Models/Menu.php
@@ -7,35 +7,10 @@ use OptimistDigital\MenuBuilder\MenuBuilder;
 
 class Menu extends Model
 {
-    protected $fillable = ['locale_parent_id'];
-
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
         $this->setTable(MenuBuilder::getMenusTableName());
-    }
-
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::deleting(function ($menu) {
-            // Is a locale parent template
-            if ($menu->locale_parent_id === null) {
-                // Find child templates
-                $childMenu = $menu::where('locale_parent_id', '=', $menu->id)->get();
-                if (count($childMenu) === 0) return;
-
-                // Pick the first template randomly and let it become the parent
-                $childMenu[0]->update(['locale_parent_id' => null]);
-                $newLocaleParentId = $childMenu[0]->id;
-
-                // Update others
-                for ($i = 1; $i < count($childMenu); $i++) {
-                    $childMenu[$i]->update(['locale_parent_id' => $newLocaleParentId]);
-                }
-            }
-        });
     }
 
     public function rootMenuItems()
