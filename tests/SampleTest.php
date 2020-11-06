fcdab89e4788dfbd3224b393dfdee57a18419b53
Allan Tatter <allan@optimistdigital.com>
2020-11-06 13:05:45 +0200

    all working

--- tests/SampleTest.php
+++ tests/SampleTest.php
@@ -10,11 +10,4 @@ class SampleTest extends TestCase
     {
         $this->assertEquals(1, 1);
     }
-
-    /** @test */
-    public function it_shows_tables()
-    {
-        $tables = json_encode(\DB::select('show tables'));
-        $this->assertEquals($tables, 'foo');
-    }
 }
