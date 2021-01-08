60389ce38554f639d1bc888d3546e1dcb677ffb0
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2021-01-08 17:56:17 +0200

    Fix build process

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,9 @@
 const mix = require('laravel-mix');
 
-mix.setPublicPath('dist').js('resources/js/menu-builder.js', 'js').vue();
-
-if (mix.inProduction()) {
-  mix.options({
-    extractVueStyles: 'css/menu-builder.css',
+mix
+  .setPublicPath('dist')
+  .js('resources/js/menu-builder.js', 'js')
+  .vue({
+    extractStyles: mix.inProduction() ? 'css/menu-builder.css' : false,
     processCssUrls: false,
   });
-}
