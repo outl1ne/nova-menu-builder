96167f279ac3a37b07c8cdbbd95c71a1d79fb324
Yuriy Martini <y.martini@soluzionesoftware.com>
2021-03-09 09:52:42 +0100

    refactored migrations

--- database/migrations/2019_11_08_000000_create_menus_table.php
+++ database/migrations/2019_11_08_000000_create_menus_table.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Schema;
 use OptimistDigital\MenuBuilder\MenuBuilder;
 
@@ -20,26 +18,7 @@ class CreateMenusTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('slug');
-            $table->string('locale');
             $table->timestamps();
-
-            $table->unique(['slug', 'locale'], 'nova_menu_menus_slug_locale_unique');
-        });
-
-        Schema::create(MenuBuilder::getMenuItemsTableName(), function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedBigInteger('menu_id')->nullable();
-            $table->string('name');
-            $table->string('class')->nullable();
-            $table->string('value')->nullable();
-            $table->string('target')->default('_self');
-            $table->json('parameters')->nullable();
-            $table->integer('parent_id')->nullable();
-            $table->integer('order');
-            $table->boolean('enabled')->default(1);
-            $table->timestamps();
-
-            $table->foreign('menu_id')->references('id')->on(MenuBuilder::getMenusTableName())->onDelete('cascade');
         });
     }
 
@@ -50,7 +29,6 @@ class CreateMenusTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists(MenuBuilder::getMenuItemsTableName());
         Schema::dropIfExists(MenuBuilder::getMenusTableName());
     }
 }
