ebf55fb9a363900b8100b3dc722943802b22ef6a
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2021-03-26 15:25:47 +0200

    Consolidate everything into one migration

--- database/migrations/2019_11_08_000000_create_menus_table.php
+++ database/migrations/2019_11_08_000000_create_menus_table.php
@@ -1,17 +1,12 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
 use OptimistDigital\MenuBuilder\MenuBuilder;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateMenusTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create(MenuBuilder::getMenusTableName(), function (Blueprint $table) {
@@ -20,15 +15,28 @@ class CreateMenusTable extends Migration
             $table->string('slug');
             $table->timestamps();
         });
+
+        Schema::create(MenuBuilder::getMenuItemsTableName(), function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('menu_id')->nullable();
+            $table->string('name');
+            $table->string('locale');
+            $table->string('class')->nullable();
+            $table->string('value')->nullable();
+            $table->string('target')->default('_self');
+            $table->json('data')->nullable();
+            $table->integer('parent_id')->nullable();
+            $table->integer('order');
+            $table->boolean('enabled')->default(1);
+            $table->timestamps();
+
+            $table->foreign('menu_id')->references('id')->on(MenuBuilder::getMenusTableName())->onDelete('cascade');
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
+        Schema::dropIfExists(MenuBuilder::getMenuItemsTableName());
         Schema::dropIfExists(MenuBuilder::getMenusTableName());
     }
 }
