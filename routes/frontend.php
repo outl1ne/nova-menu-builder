567bef3ecca03300ae4bb200fcd3c5b40b64db69
Riccardo Slanzi <rslanzi@gmail.com>
2021-12-15 15:59:13 +0100

    Update frontend.php

--- routes/frontend.php
+++ routes/frontend.php
@@ -5,5 +5,8 @@ use Workup\MenuBuilder\Http\Controllers\Frontend\MenuController;
 use Workup\MenuBuilder\Http\Controllers\Frontend\MenuItemController;
 
 Route::get('/menus', [MenuController::class, 'index']);
-Route::get('/menus/{menu}', [MenuController::class, 'show']);
-Route::get('/menus/{menu}/{slug}', [MenuItemController::class, 'show']);
+Route::get('/menus/{menu}', [MenuController::class, 'show'])
+    ->whereNumber('menu');
+Route::get('/menus/{menu}/{slug}', [MenuItemController::class, 'show'])
+    ->whereNumber('menu')
+    ->whereAlphaNumeric('slug');
