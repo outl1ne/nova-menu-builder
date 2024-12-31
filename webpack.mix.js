0753f5b65dcd132101a7b38c3eb7560dac5a689b
Tarvo R. <TarvoReinpalu@gmail.com>
2024-12-31 14:12:43 +0200

    Nova V5 initial support

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,18 +3,13 @@ let mix = require('laravel-mix');
 let postcss = require('postcss');
 let tailwindcss = require('tailwindcss');
 
+mix.extend('nova', new require('laravel-nova-devtool'));
+
 mix
   .setPublicPath('dist')
   .js('resources/js/entry.js', 'js')
   .vue({ version: 3 })
-  .webpackConfig({
-    externals: {
-      vue: 'Vue',
-    },
-    output: {
-      uniqueName: 'outl1ne/nova-menu-builder-runner',
-    },
-  })
+  .nova('outl1ne/nova-multiselect-field')
   .postCss('resources/css/entry.css', 'dist/css/', [postcss(), tailwindcss('tailwind.config.js')])
   .alias({
     'laravel-nova': path.join(__dirname, 'vendor/laravel/nova/resources/js/mixins/packages.js'),
